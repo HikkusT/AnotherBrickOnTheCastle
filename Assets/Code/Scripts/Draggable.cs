094aa91271dea7ce4abeb21b95954db95a71e332
HikkusT <luizhenrique98@gmail.com>
2020-02-01 19:34:13 -0300

    Initial commit 2

---
+++ Assets/Code/Scripts/Draggable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector3 savePos;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        savePos = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData) 
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = savePos;
+    }
+}
