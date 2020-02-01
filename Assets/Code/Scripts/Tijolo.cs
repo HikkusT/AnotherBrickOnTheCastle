9245d9d502b749c540f871e6f51edc9ffdf18c20
Larissa <larissastefano12@gmail.com>
2020-02-01 18:32:54 -0300

    iniciei o script do objeto tijolo

---
+++ Assets/Code/Scripts/Tijolo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tijolo : MonoBehaviour
+{
+    Vector3 direction;
+    public float speed;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Vector3.right;
+        velocity = speed * direction.normalized;
+        this.transform.position = this.transform.position + velocity * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        direction = -direction;
+    }
+}
