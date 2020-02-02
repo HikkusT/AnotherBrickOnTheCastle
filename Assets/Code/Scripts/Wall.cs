38ea3ab833f3a03d36a29be13615ceb6b7e7b7f8
HikkusT <luizhenrique98@gmail.com>
2020-02-02 03:46:46 -0300

    BORAA

--- Assets/Code/Scripts/Wall.cs
+++ Assets/Code/Scripts/Wall.cs
@@ -7,29 +7,68 @@ public class Wall : MonoBehaviour
     public float wallWidth;
     public int numOfBricks;
     public GameObject brick;
-    private float desiredBrickWidth;
-    private float screenWidth;
+    public float brickVariation;
+
     private float brickWidth;
+    private Queue<Vector2> holesPositions = new Queue<Vector2>();
+    private Queue<Vector3> scales = new Queue<Vector3>();
+    private float desiredBrickWidth;
+    private float scaleMultiplier;
 
     // Start is called before the first frame update
     void Start()
     {
-        desiredBrickWidth = wallWidth / numOfBricks;
         brickWidth = brick.GetComponent<SpriteRenderer>().sprite.rect.width;
-        float scaleMultiplier = desiredBrickWidth / brickWidth;
-
-        for(int i = 0; i < numOfBricks; i ++)
-        {
-            GameObject brickInstance = Instantiate(brick, 
-                                                  new Vector3((i * desiredBrickWidth + desiredBrickWidth / 2 - wallWidth/2)/100, 0, 0),
-                                                  Quaternion.identity);
-            brickInstance.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1);
-        }
+        GenerateWall();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+
+    private void GenerateWall()
+    {
+        desiredBrickWidth = wallWidth / numOfBricks;
+        scaleMultiplier = desiredBrickWidth / brickWidth;
+        float brickHeight = scaleMultiplier * brick.GetComponent<SpriteRenderer>().sprite.rect.height;
+
+        for (int i = 0; i < 200 * Camera.main.orthographicSize / brickHeight; i++)
+            GenerateRow((i * brickHeight + brickHeight / 2) / 100 - Camera.main.orthographicSize);
+    }
+
+    private void GenerateRow(float height)
+    {
+        float startPos = (-wallWidth / 2) / 100;
+        float currentLength = 0;
+        int chosenTile = Random.Range(0, numOfBricks);
+        
+        for (int i = 0; i < numOfBricks; i++)
+        {
+            float variation = Random.Range(-brickVariation, brickVariation);
+            float currentBrickWidth = desiredBrickWidth * (1 + variation);
+
+            if (i == numOfBricks - 1)
+                currentBrickWidth = wallWidth - currentLength;
+
+            currentLength += (currentBrickWidth / 2);
+            float xPos = startPos + currentLength / 100;
+            float scaleMultiplierX = currentBrickWidth / brickWidth;
+            if (i != chosenTile)
+            {
+                GameObject brickInstance = Instantiate(brick, new Vector2(xPos, height), Quaternion.identity);
+
+                brickInstance.transform.localScale = new Vector3(scaleMultiplierX, scaleMultiplier, 1);
+            }
+            else
+            {
+                holesPositions.Enqueue(new Vector2(xPos, height));
+                scales.Enqueue(new Vector3(scaleMultiplierX, scaleMultiplier, 1));
+            }
+            currentLength += (currentBrickWidth / 2);
+        }
     }
 }
